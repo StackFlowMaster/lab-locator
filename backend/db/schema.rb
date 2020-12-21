dd8643825be0919ac9ae9e309c7a950c1323d8e4
StackFlowMaster <acedev709@gmail.com>
2020-12-21 01:14:01 -0500

    adds user model with secure password

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_024657) do
+ActiveRecord::Schema.define(version: 2020_12_21_061016) do
 
   create_table "studios", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_12_21_024657) do
     t.string "image_url"
     t.integer "hourly_rate"
     t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
